f41da566dcdbcc8b25ae3b84d9cfe4d476677c05
avishekGit2024 <patraavishek881@gmail.com>
2025-02-12 20:45:45 +0530

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 'use client'
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1 className="text-xl text-center mt-10">this is true feedback</h1>
+    <div>
+      <div className="flex mt-20 items-center justify-center">
+      <h1 className="text-xl text-center ">Start your Anonymous adventure with Us..</h1>
+      <Button  className="text-xl text-center ml-1" variant="default"><Link href="/sign-in">Start for free</Link></Button>
+      </div>
+    </div>
   );
 }
