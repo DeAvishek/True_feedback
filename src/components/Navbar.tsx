a5091689c1bba7161c2312a2e606e75a4eb8b353
avishekGit2024 <patraavishek881@gmail.com>
2025-02-15 19:44:52 +0530

    navbar status

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { useSession ,signOut} from 'next-auth/react'
 import { User } from 'next-auth'
 import { Button } from './ui/button';
 import Link from 'next/link';
+
 const Navbar = () => {
     const {data:session}=useSession();
     const user=session?.user as User 
@@ -14,22 +15,15 @@ const Navbar = () => {
             <h1 className="text-2xl font-bold cursor-pointer">Mystrey Message</h1>
         </Link>
         <div>
-            {session ? (
-                <div className="flex items-center gap-4">
+           
+                {user && <div className="flex items-center gap-4">
                     <span className="text-sm md:text-base">Welcome, {user.email}</span>
                     <Button 
-                        onClick={() => signOut()} 
+                        onClick={() => signOut({callbackUrl:"/sign-in"})} 
                         className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md">
                         Log out
                     </Button>
-                </div>
-            ) : (
-                <Link href="/sign-in">
-                    <Button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md">
-                        Sign In
-                    </Button>
-                </Link>
-            )}
+                </div>}
         </div>
     </div>
 </nav>
